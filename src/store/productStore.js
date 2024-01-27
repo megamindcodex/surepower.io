3588246dbbb33c05324260e78d6321dc19c986c4
Emmancodex <megamindcodex@gmail.com>
2024-01-28 00:53:28 +0100

    semi fixed

--- src/store/productStore.js
+++ src/store/productStore.js
@@ -15,7 +15,7 @@ export const useProductsStore = defineStore("productStore", {
     async getAllProducts() {
       try {
         this.loading = true;
-        const res = await axios.get(`${this.endpoint}/api/products`);
+        const res = await axios.get(`${endpoint}/api/products`);
         if (res.status === 200) {
           const data = res.data;
           this.products = data;
